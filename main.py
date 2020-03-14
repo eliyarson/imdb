6c18eb56eb50958388a77f06c0cf745a59273ffa
Eli <eli_tgt@hotmail.com>
2020-03-14 08:34:08 -0700

    added slider and linked output to DataTable

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import pandas as pd
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_functions
+import dash_table
+from dash.dependencies import Input,Output
 
 #import scrape function
 import scrape
 
+df = scrape.scrape()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Flask(__name__)
-df = scrape.scrape()
+
 
 #dash app
 dash_app = dash.Dash(
@@ -25,10 +27,29 @@ dash_app = dash.Dash(
 
 dash_app.layout = html.Div(
     [
-    html.H4(children='Top 100 Movies'),
-    dash_functions.generate_table(df,100)
+    html.H4(children='Top 1000 Movies'),
+    html.Label('Slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df.year.min(),
+        max=df.year.max(),
+        marks={i: 'Year {}'.format(i) if i == df.year.min() else str(i) for i in range(df.year.min(), df.year.max()+1,10)},
+        value=2019
+    ),
+    dash_table.DataTable(id='table',
+    columns=[{"name":i,"id":i} for i in df.columns]
+    )
     ]
 )
+
+@dash_app.callback(
+    Output('table','data'),
+    [Input('year-slider','value')]
+)
+def update_df(input_year):
+    filtered_df = df[df.year==input_year].sort_values(by='imdb_ratings',ascending=False)
+    filtered_data = filtered_df.to_dict('records')
+    return filtered_data
 
 #scraped dataframe
 @app.route('/raw/')  
@@ -38,7 +59,7 @@ def home():
 #main page
 @app.route('/')
 def main() :
-    return "Welcome to Flask "
+    return "Welcome to my Internet Movies DashBoard "
     
 if __name__ == '__main__':
     dash_app.run_server()
