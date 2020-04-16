84497afe735299172ed6af1e0c74944dfd560dbf
Eli <eli.yarson@gmail.com>
2020-04-15 21:27:52 -0300

    Added a html template for future modifications

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,10 +1,12 @@
 #flask_app.py
-from flask import Flask
+from flask import Flask, render_template
 
 flask_app = Flask(__name__)
 
 #home page
 @flask_app.route('/')
-def index() :
-    return "Welcome to my Internet Movies DashBoard "
+def index():
+    return render_template('simple.html')
 
+if __name__=='__main__':
+    flask_app.run(debug=True)
